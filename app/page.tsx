4c2ff261d8558b71aef9d06e53ced0efdafd7bba
tinahung427-lian <tinahung427@gmail.com>
2026-04-07 23:12:18 +0800

    feat: HW1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import Folder from '@/components/Folder'
 
 export default function Home() {
   return (
-   <div>YA</div>
+    <>
+    首頁
+    <div style={{ height: '600px', position: 'relative' }}>
+      <Folder size={2} color="#5227FF" className="custom-folder"/>
+    </div>
+  </>
   );
 }
