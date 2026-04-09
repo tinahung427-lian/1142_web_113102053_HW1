0e4de587d8ba2be3b0af3e37abc9fbf35b669ace
tinahung427-lian <tinahung427@gmail.com>
2026-04-09 11:45:33 +0800

    feat:HW1

--- app/graphic-design/page.tsx
+++ app/graphic-design/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
     return (
       <div>
-        <h1>graphic design</h1>
+        <h1>Graphic Design</h1>
         <p>Ai、Ps、procreat</p>
       </div>
     );
