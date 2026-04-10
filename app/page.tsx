006c5d6ca855b94501ec7fd6a4a0be23f2387c40
tinahung427-lian <tinahung427@gmail.com>
2026-04-10 11:41:03 +0800

    feat:HW1

--- app/page.tsx
+++ app/page.tsx
@@ -16,27 +16,27 @@ export default function Home() {
           mouse={0.1}
           strength={0.15}
           relaxation={0.9}
-          offsetY={0.5}
+          offsetY={0.3}
           className="absolute inset-0"
         />
       </div>
 
-      <div className="absolute top-10 left-10 z-10 text-white text-4xl font-bold">
+      <div className="absolute top-[80%] left-[80%] text-white text-2xl font-semibold">
         HELLO
       </div>
 
       {/* About Me 連結 */}
       <a
         href="/about"
-        className="absolute z-10 text-white text-xl font-bold
+        className="absolute z-10 text-white text-5xl font-bold
                   hover:underline hover:scale-110 transition-all duration-300"
-        style={{ top: "40%", left: "50%"}}
+        style={{ top: "30%", left: "38%"}}
       >
         About Me 
       </a>
 
-      <div className="absolute top-[60%] left-[10%] text-white text-4xl font-semibold">
-        HUNG HSIU-LIAN
+      <div className="absolute top-[65%] left-[5%] text-white text-xl font-semibold">
+        HUNG HSIU-LIAN 
       </div>
 
 
