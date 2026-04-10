24b2d1f13603fe0d0bfb76edea69276b665319eb
tinahung427-lian <tinahung427@gmail.com>
2026-04-11 03:26:44 +0800

    feat: HW1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,27 @@
 import PixelTransition from '@/components/PixelTransition';
+import { ImStarEmpty } from "react-icons/im";
+import { ImStarFull } from "react-icons/im";
+import { TbBrandAdobeIllustrator } from "react-icons/tb";
+import { TbBrandAdobePhotoshop } from "react-icons/tb";
+import { IoLogoFigma } from "react-icons/io5";
+import { SiBlender } from "react-icons/si";
+import { TbBrandAdobePremier } from "react-icons/tb";
+
+
+
+function Stars({ count }: { count: number }){
+  return (
+    <div className="flex gap-1 text-[#1f2a44]">
+      {[1, 2, 3, 4, 5].map((i) =>
+        i <= count ? (
+          <ImStarFull key={i} />
+        ) : (
+          <ImStarEmpty key={i} />
+        )
+      )}
+    </div>
+  );
+}
 
 export default function AboutPage() {
   return (
@@ -57,13 +80,72 @@ export default function AboutPage() {
         </div>
 
 
-        <div className="col-span-2 row-span-2 col-start-3 row-start-1 rounded-3xl bg-white flex items-center justify-center">
-          02
+        <div className="col-span-2 row-span-2 col-start-3 row-start-1 rounded-3xl bg-white p-6 overflow-x-auto overflow-y-hidden">
+          <div className="min-w-[700px] leading-relaxed">
+            <p className="font-bold mb-2">◾ Education</p>
+            <div className="pl-5 mb-4">
+              <p>-國立政治大學 教育學系</p>
+              <p>-國立政治大學 數位內容與科技學士學位學程</p>
+            </div>
+
+            <p className="font-bold mb-2">◾ Experience</p>
+            <div className="pl-5">
+              <p>-國科會補助研究計畫：運動員生涯轉銜培力計畫（兼任人力）2025.12–2026.01</p>
+              <p>-TED分享社－行銷部 114學年</p>
+              <p>-數位平台－見習助理_攝影組長 2026.02–</p>
+            </div>
+          </div>
         </div>
 
-        <div className="col-span-2 row-span-2 col-start-3 row-start-3 rounded-3xl bg-white flex items-center justify-center">
-          03
+        <div className="col-span-2 row-span-2 col-start-3 row-start-3 rounded-3xl bg-white p-6 leading-relaxed relative overflow-hidden">
+          <p className="font-bold mb-4">◾ Skill</p>
+          <div className="space-y-3 pl-3">
+
+            <div className="flex items-center gap-4">
+              <span className="w-[140px]">-Illustrator</span>
+              <Stars count={4} />
+              
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span className="w-[140px]">-Photoshop</span>
+              <Stars count={4} />
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span className="w-[140px]">-Premiere</span>
+              <Stars count={2} />
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span className="w-[140px]">-Figma</span>
+              <Stars count={3} />
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span className="w-[140px]">-Blender</span>
+              <Stars count={2} />
+            </div>
+
+          </div>
+
+          <div className="absolute right-6 bottom-6 flex flex-col items-end gap-3 text-3xl text-slate-600 opacity-80">
+            
+            <div className="flex gap-2 mr-5">
+              <IoLogoFigma className="transition hover:-translate-y-1" />
+              <SiBlender className="transition hover:-translate-y-1" />
+              
+            </div>
+
+            <div className="flex gap-2">
+              <TbBrandAdobePhotoshop className="transition hover:-translate-y-1" />
+              <TbBrandAdobeIllustrator className="transition hover:-translate-y-1" />
+              <TbBrandAdobePremier className="transition hover:-translate-y-1" />
+            </div>
+          </div>
+
         </div>
+
 
         <div className="col-span-4 row-span-2 row-start-5 rounded-3xl bg-white flex items-center justify-center">
           04
@@ -74,5 +156,4 @@ export default function AboutPage() {
     </div>
   );
 }
-
 
