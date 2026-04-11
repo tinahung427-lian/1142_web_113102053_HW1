a1cd5db505217d77c6779aa292c9a19c33f82044
tinahung427-lian <tinahung427@gmail.com>
2026-04-11 20:53:07 +0800

    feat:HW1

--- app/photography/scenery/page.tsx
+++ app/photography/scenery/page.tsx
@@ -1,3 +1,6 @@
+import Masonry from '@/components/Masonry';
+
+
 export default function SceneryPage() {
     return (
       <div className="p-6 text-white">
@@ -5,3 +8,38 @@ export default function SceneryPage() {
       </div>
     );
   }
+
+  const items = [
+      {
+        id: "1",
+        img: "https://picsum.photos/id/1015/600/900?grayscale",
+        url: "https://example.com/one",
+        height: 400,
+      },
+      {
+        id: "2",
+        img: "https://picsum.photos/id/1011/600/750?grayscale",
+        url: "https://example.com/two",
+        height: 250,
+      },
+      {
+        id: "3",
+        img: "https://picsum.photos/id/1020/600/800?grayscale",
+        url: "https://example.com/three",
+        height: 600,
+      },
+      // ... more items
+  ];
+  
+  <Masonry
+    items={items}
+    ease="power3.out"
+    duration={0.6}
+    stagger={0.05}
+    animateFrom="bottom"
+    scaleOnHover
+    hoverScale={0.95}
+    blurToFocus
+    colorShiftOnHover={false}
+  />
+  
